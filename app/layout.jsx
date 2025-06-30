9c1a598314ba6461feded27384bddf7d3e12de29
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-06-30 18:01:54 +0100

    add Libre_Baskerville font.
    add class of 'container' to body tag.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,23 @@
 import '@/app/ui/global.css';
+import Header from '@/components/Header';
+
+import {Libre_Baskerville} from 'next/font/google';
+
+const libreBaskerville = Libre_Baskerville({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    variable: '--ff-libre-baskerville',
+    display: 'swap',
+});
 
 export default function RootLayout({children}) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${libreBaskerville.variable}`}>
             <title>Galleria Slideshow Site</title>
-            <body>{children}</body>
+            <body className="container">
+                <Header />
+                {children}
+            </body>
         </html>
     );
 }
