8fac5a27285015002361dd6edcf13f53299d7fb4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-02 10:35:39 +0100

    use Masonry from react-masonry-css to render home page image-cards

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,12 +1,25 @@
+'use client';
+
 import ImageCard from '@/components/ImageCard';
 import data from '@/lib/data.json';
+import React from 'react';
+import Masonry from 'react-masonry-css';
+
+const breakpointColumnsObj = {
+    default: 4,
+    760: 2,
+    425: 1,
+};
 
 export default function HomePage() {
     return (
-        <>
+        <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="multicol"
+            columnClassName="gallery-column">
             {data.map((item) => {
                 return <ImageCard key={item.name} details={item} />;
             })}
-        </>
+        </Masonry>
     );
 }
