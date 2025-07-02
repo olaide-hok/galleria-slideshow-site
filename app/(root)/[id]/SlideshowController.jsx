1fdcb43661ce648c50cb0d01ee672240eee63df4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-02 10:33:49 +0100

    update attribute names

--- app/(root)/[id]/SlideshowController.jsx
+++ app/(root)/[id]/SlideshowController.jsx
@@ -18,10 +18,10 @@ const SlideshowController = ({artName, artistName}) => {
                             width="26"
                             height="24"
                             xmlns="http://www.w3.org/2000/svg">
-                            <g stroke="#000" fill="none" fill-rule="evenodd">
+                            <g stroke="#000" fill="none" fillRule="evenodd">
                                 <path
                                     d="M24.166 1.843L3.627 12.113l20.539 10.269V1.843z"
-                                    stroke-width="2"
+                                    strokeWidth="2"
                                 />
                                 <path fill="#D8D8D8" d="M.986.5h-1v22.775h1z" />
                             </g>
@@ -32,10 +32,10 @@ const SlideshowController = ({artName, artistName}) => {
                             width="26"
                             height="24"
                             xmlns="http://www.w3.org/2000/svg">
-                            <g stroke="#000" fill="none" fill-rule="evenodd">
+                            <g stroke="#000" fill="none" fillRule="evenodd">
                                 <path
                                     d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z"
-                                    stroke-width="2"
+                                    strokeWidth="2"
                                 />
                                 <path
                                     fill="#D8D8D8"
