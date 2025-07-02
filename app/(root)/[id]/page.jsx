82c4a10f9fa9f68c4f23584c358cf9feb20e06d9
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-02 07:18:35 +0100

    render SlideshowController
    wrap layout a div.

--- app/(root)/[id]/page.jsx
+++ app/(root)/[id]/page.jsx
@@ -1,4 +1,5 @@
 import data from '@/lib/data.json';
+import SlideshowController from './SlideshowController';
 
 const getGalleryDetails = (id) => {
     return data.find((item) => item.id === id);
@@ -26,70 +27,74 @@ async function DetailsPage({params}) {
     } = details;
 
     return (
-        <div className="details-wrapper flex">
-            {/* Main Gallery */}
-            <div className="gallery-wrapper">
-                {/* hero large */}
-                <div className="hero-wrapper">
-                    <button
-                        className="view-image-btn flex uppercase"
-                        type="button">
-                        <img
-                            src="./assets/shared/icon-view-image.svg"
-                            alt="icon-view-image"
-                        />
-                        <span className="fs-10 text-white fw-700 letter-spacing-3">
-                            View image
-                        </span>
-                    </button>
-                    <img className="hero-img" src={large} alt={artName} />
+        <div className="details-container">
+            <div className="details-wrapper flex">
+                {/* Main Gallery */}
+                <div className="gallery-wrapper">
+                    {/* hero large */}
+                    <div className="hero-wrapper">
+                        <button
+                            className="view-image-btn flex uppercase"
+                            type="button">
+                            <img
+                                src="./assets/shared/icon-view-image.svg"
+                                alt="icon-view-image"
+                            />
+                            <span className="fs-10 text-white fw-700 letter-spacing-3">
+                                View image
+                            </span>
+                        </button>
+                        <img className="hero-img" src={large} alt={artName} />
+                    </div>
+
+                    {/* hero small */}
+                    {/* <img src="" alt="" /> */}
+
+                    {/* Artist Details */}
+
+                    <div className="artist-details-wrapper">
+                        <div className="artist-details">
+                            <div className="artist-info flex">
+                                <p className="art-name text-preset-2 fw-700 text-black">
+                                    {artName}
+                                </p>
+                                <p className="lh-135 fs-15 text-grey-400">
+                                    {artistName}
+                                </p>
+                            </div>
+                            <img
+                                className="artist-img"
+                                src={artistImage}
+                                alt={artistName}
+                            />
+                        </div>
+                    </div>
                 </div>
 
-                {/* hero small */}
-                {/* <img src="" alt="" /> */}
+                {/* Description, year, and source */}
+                <div className="details-copy grid">
+                    <p className="year text-grey-100 text-preset-1 fw-700">
+                        {year}
+                    </p>
 
-                {/* Artist Details */}
-
-                <div className="artist-details-wrapper">
-                    <div className="artist-details">
-                        <div className="artist-info flex">
-                            <p className="art-name text-preset-2 fw-700 text-black">
-                                {artName}
-                            </p>
-                            <p className="lh-135 fs-15 text-grey-400">
-                                {artistName}
-                            </p>
-                        </div>
-                        <img
-                            className="artist-img"
-                            src={artistImage}
-                            alt={artistName}
-                        />
+                    <div className="desc-source-wrapper flex">
+                        <p className="description fs-14 text-grey-400 fw-700 ls-2 lh-200">
+                            {description}
+                        </p>
+                        <a
+                            className="source"
+                            href={source}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <span className="text-grey-400 fs-9 fw-700 ls-3 lh-125 uppercase">
+                                go to source
+                            </span>
+                        </a>
                     </div>
                 </div>
             </div>
-
-            {/* Description, year, and source */}
-            <div className="details-copy grid">
-                <p className="year text-grey-100 text-preset-1 fw-700">
-                    {year}
-                </p>
-
-                <div className="desc-source-wrapper flex">
-                    <p className="description fs-14 text-grey-400 fw-700 ls-2 lh-200">
-                        {description}
-                    </p>
-                    <a
-                        className="source"
-                        href={source}
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        <span className="text-grey-400 fs-9 fw-700 ls-3 lh-125 uppercase">
-                            go to source
-                        </span>
-                    </a>
-                </div>
-            </div>
+            {/* Slideshow Controller */}
+            <SlideshowController artName={artName} artistName={artistName} />
         </div>
     );
 }
