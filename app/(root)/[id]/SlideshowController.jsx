b9507d7b59dbc7c61a6e2eb7604d13c247348dec
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-03 18:53:11 +0100

    render ProgressIndicator
    conditionally render next and prev buttons as link or button tags.

--- app/(root)/[id]/SlideshowController.jsx
+++ app/(root)/[id]/SlideshowController.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+'use client';
+import Link from 'next/link';
+import ProgressIndicator from './ProgressIndicator';
 
-const SlideshowController = ({artName, artistName}) => {
+const SlideshowController = ({
+    artName,
+    artistName,
+    totalArtworks,
+    index,
+    nextId,
+    prevId,
+}) => {
+    const isFirst = index === 0;
+    const isLast = index === totalArtworks - 1;
+
     return (
         <div className="slideshow-controller-wrapper">
-            <div className="slideshow-progress-indicator"></div>
+            <ProgressIndicator currentIndex={index} total={totalArtworks} />
             <div className="slide-info flex">
                 {/* art info */}
                 <div className="art-info flex">
@@ -13,37 +25,83 @@ const SlideshowController = ({artName, artistName}) => {
 
                 {/* buttons */}
                 <div className="slideshow-btns flex">
-                    <button type="button">
-                        <svg
-                            width="26"
-                            height="24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <g stroke="#000" fill="none" fillRule="evenodd">
-                                <path
-                                    d="M24.166 1.843L3.627 12.113l20.539 10.269V1.843z"
-                                    strokeWidth="2"
-                                />
-                                <path fill="#D8D8D8" d="M.986.5h-1v22.775h1z" />
-                            </g>
-                        </svg>
-                    </button>
-                    <button type="button">
-                        <svg
-                            width="26"
-                            height="24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <g stroke="#000" fill="none" fillRule="evenodd">
-                                <path
-                                    d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z"
-                                    strokeWidth="2"
-                                />
-                                <path
-                                    fill="#D8D8D8"
-                                    d="M24.708.5h1v22.775h-1z"
-                                />
-                            </g>
-                        </svg>
-                    </button>
+                    {isFirst ? (
+                        <button disabled className="disabled">
+                            <svg
+                                width="26"
+                                height="24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g stroke="#000" fill="none" fillRule="evenodd">
+                                    <path
+                                        d="M24.166 1.843L3.627 12.113l20.539 10.269V1.843z"
+                                        strokeWidth="2"
+                                    />
+                                    <path
+                                        fill="#D8D8D8"
+                                        d="M.986.5h-1v22.775h1z"
+                                    />
+                                </g>
+                            </svg>
+                        </button>
+                    ) : (
+                        <Link type="button" href={`/${prevId}`}>
+                            {' '}
+                            <svg
+                                width="26"
+                                height="24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g stroke="#000" fill="none" fillRule="evenodd">
+                                    <path
+                                        d="M24.166 1.843L3.627 12.113l20.539 10.269V1.843z"
+                                        strokeWidth="2"
+                                    />
+                                    <path
+                                        fill="#D8D8D8"
+                                        d="M.986.5h-1v22.775h1z"
+                                    />
+                                </g>
+                            </svg>
+                        </Link>
+                    )}
+
+                    {/* Next */}
+                    {isLast ? (
+                        <button disabled className="disabled">
+                            <svg
+                                width="26"
+                                height="24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g stroke="#000" fill="none" fillRule="evenodd">
+                                    <path
+                                        d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z"
+                                        strokeWidth="2"
+                                    />
+                                    <path
+                                        fill="#D8D8D8"
+                                        d="M24.708.5h1v22.775h-1z"
+                                    />
+                                </g>
+                            </svg>
+                        </button>
+                    ) : (
+                        <Link type="button" href={`/${nextId}`}>
+                            <svg
+                                width="26"
+                                height="24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g stroke="#000" fill="none" fillRule="evenodd">
+                                    <path
+                                        d="M1.528 1.843l20.538 10.27L1.528 22.382V1.843z"
+                                        strokeWidth="2"
+                                    />
+                                    <path
+                                        fill="#D8D8D8"
+                                        d="M24.708.5h1v22.775h-1z"
+                                    />
+                                </g>
+                            </svg>
+                        </Link>
+                    )}
                 </div>
             </div>
         </div>
